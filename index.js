7f46c9c05fb62f05c10e6203a184a4927b5d2c57
boppe123 <44640763+boppe123@users.noreply.github.com>
2018-11-05 14:54:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ restService.post("/webhooktest", function(req, res) {
  let Unit = req.body.queryResult.parameters['Unit']; // take out the Unit, lamp e.g.
  let state = req.body.queryResult.parameters['state']; // take out the the state, on or off
  let cmd = req.body.queryResult.parameters['cmd'];
+ if (Unit == 'state'){
+	   callThingApi().then((output) => {
+    res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
+  }).catch(() => {
+    res.json({ 'fulfillmentText': 'something is wrong' });
+  });
+ }
  if (Unit == 'state' && state == 'on' && cmd == 'turn'){
 	 callThingApiON().then((output) => {
     res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
@@ -43,6 +50,35 @@ restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
 
+
+
+function callThingApi () {
+    return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
+    // Make the HTTP request
+	
+    https.get('https://api.thingspeak.com/channels/592740/feeds.json?results=2', (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        let response = JSON.parse(body);
+        let temp = response.feeds[0].field1;
+        // Create response
+        let output = 'The temperature is ' + temp + ' degree';
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log('Error calling API')
+        reject();
+      });
+    });
+  });
+}
 
 
 
